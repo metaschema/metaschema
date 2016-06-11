3a1b589408ba25596e8407aaf9bdfe32cbbca45a
Hideki Yamamoto <hideki.yamamoto@gmail.com>
2016-06-11 06:32:24 +0200

    update

---
+++ app.js
@@ -0,0 +1,69 @@
+window.gid=function(id){return document.getElementById(id);}
+window.app={loggedin:false,dbCollections:[],
+	init:function(){var _this=this;f$.initAuth();
+		firebase.auth().onAuthStateChanged(function(user){gid('prelogindiv').style.display='none';
+			if(user){_this.loggedinit(user);gid('metaschema-app').style.display='';gid('logindiv').style.display='none';}
+			else{gid('metaschema-app').style.display='none';gid('logindiv').style.display='';console.log('log off');}
+	});},
+	login:function(provider){f$.login(provider);},logout:function(provider){f$.logout();},
+	loggedinit:function(user){console.log('ok');
+		var uname=user.email;
+		if(user.displayName){uname=user.displayName;}
+		document.getElementById('username').innerHTML=' '+uname;
+		this.scandb();
+					//if user is not agreed make him agree to terms
+					//ELSE
+		firebase.database().ref('/tag/root').set({"doctitle":"root"});
+		this.refreshtree();
+					//if not hidewelcomedialog show welcome dialog / assistent
+		},
+		scandb:function(){this.dbCollections=[];firebase.database().ref('/').on('child_added',app._scandb);},
+		_scandb:function(d){var k=d.key;if(!(k.indexOf(f$.oxyprefix)==0)){app.dbCollections[app.dbCollections.length]=d.key;}},
+		toggletree:function(button){
+			document.body.classList.toggle('noleft');
+			button.classList.toggle('pushed');
+			button.childNodes[0].classList.toggle('fa-dedent');
+			button.childNodes[0].classList.toggle('fa-indent');
+		},
+		refreshtree:function(){gid('tag-tree').innerHTML='';
+			firebase.database().ref('/tag').orderByChild("parent").startAt('root').endAt('root').on('child_added',function(snap){var v=snap.val();v.$key='tag-'+snap.key;
+					var s='<div class="treenode" id="T'+v.$key
+					s+='"><a href="#" onclick="app.leaftoggle(\''+v.$key+'\',this.firstChild)"><i class="fa fa-plus-circle"></i></a>';
+					s+='<a href="#" onclick="app.open(\''+v.$key+'\');">'+v.doctitle+'</a>';
+					s+='<a class="color" href="#" onclick="" style="background-color:#ffffff;"> </a></div>';
+					gid('tag-tree').innerHTML+=s;
+		});},
+		leaftoggle:function($key,button){var k=$key.replace('tag-','');var flag=true;button.classList.toggle('fa-plus-circle');button.classList.toggle('fa-minus-circle');
+			var leaf=gid('T'+$key).getElementsByTagName('div');
+			if(leaf.length<1){gid('T'+$key).innerHTML+='<div id="TC'+$key+'" class="treeleaf"> </div>'}
+			else{leaf=leaf[0];if(leaf.style.display!='none'){leaf.style.display='none';flag=false;}else{leaf.style.display='';gid('TC'+$key).innerHTML='';}}
+			if(flag){
+			firebase.database().ref('/tag').orderByChild("parent").startAt(k).endAt(k).on('child_added',function(snap){var v=snap.val();v.$key='tag-'+snap.key;
+					var s='<div class="treenode" id="T'+v.$key
+					s+='"><a href="#" onclick="app.leaftoggle(\''+v.$key+'\',this.firstChild)"><i class="fa fa-plus-circle"></i></a>';
+					s+='<a href="#" onclick="app.open(\''+v.$key+'\')">'+v.doctitle+'</a>';
+					s+='<a class="color" href="#" onclick="" style="background-color:#ffffff;"> </a></div>';
+					gid('TC'+$key).innerHTML+=s;
+		});}},
+		add_tag:function(){
+			var n=prompt('insert name of new tag');
+			if(n){
+				f$.db.add('tag',{parent:"root",doctitle:n});
+		}},
+			 search:function(exp,_collection){gid('results').innerHTML='';f$.db.find(exp,app._search,_collection);},
+			 _search:function(d){var s='<input onclick="app.open(\''+d.$key+'\');" type="button" value="'+d.title+' ['+d.$key+']" />';gid('results').innerHTML+=s;},
+			 open:function($key){f$.db.getone($key,app._open);},
+			 _open:function(d){app.nonewUI(d.$key);gid('details').value=JSON.stringify(d);},
+								seelog_clicked:function(){
+				 gid('subcollections').innerHTML='';					
+					var k=gid('dockey').value;},
+				seever_clicked:function(){
+					gid('subcollections').innerHTML='';
+					var k=gid('dockey').value;},			
+				save_clicked:function(){
+					var e=gid('dockey');
+					var J=JSON.parse(gid('details').value);
+					if(e.value=='new doc'){app.nonewUI(f$.db.add(gid('collection').value,J));}
+					else{if(!J.$key){alert('$key property must be present in the document');}else{f$.db.set(J)}}},
+
+			};
--- firebase-oxygen.js
+++ firebase-oxygen.js
@@ -6,7 +6,7 @@
 	-Actually perform  Z=Y-X  during the indexing to remove old entries. 
 	-CLEAN INDEXES ON DOCUMENT REMOVAL
 */
-f$={oxyprefix:'oxy',
+f$={oxyprefix:'oxy_',
 inoe:function(v){if(!v)return true;if(typeof v!='string')return true;if(v.length==0)return true;return false;},
 login:function(provider,method){if(!method){method='redirect'}
 	if (!firebase.auth().currentUser){var provider;
@@ -18,14 +18,11 @@ login:function(provider,method){if(!method){method='redirect'}
 	}else{console.log('Already logged in');}	
 },
 logout:function(){firebase.auth().signOut();},
-initAuth:function(nextToken){
- firebase.auth().getRedirectResult().then(function(result){
- if(result.credential){nextToken(result.credential.accessToken);}else{
-									nextToken(false);
-        }								
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function(error) {
+initAuth:function(nextToken){if(!nextToken){nextToken=function(r){var i=0;}}firebase.auth().getRedirectResult().then(function(result){
+ if(result.credential){nextToken(result);}
+	else{nextToken(false);}
+		var user = result.user;
+  }).catch(function(error) {
         var errorCode = error.code;var errorMessage = error.message;
         var email = error.email;
         var credential = error.credential;
@@ -38,7 +35,7 @@ initAuth:function(nextToken){
 },
 
 	/*-----------FIREBASE DATABASE NAMESPACE - start----------------*/
-db:{db:function(ref){return firebase.database().ref(ref)},
+db:{docnamefield:"doctitle",db:function(ref){return firebase.database().ref(ref)},
 	start:function(key,event,next){this.db(key.replace('-','/-')).on(event,function(d){var v=d.val();if(v){v.$key=key.split('-')[0]+d.key;next(v);}});},
 	end:function(key,event){this.db(key.replace('-','/-')).off(event);},
 	/*SUBCOLLECTION start*/
@@ -49,60 +46,75 @@ db:{db:function(ref){return firebase.database().ref(ref)},
 	_getone:function(stype,dkey,key,next){this.db(stype+'_'+dkey.replace('-','/-')+'/'+key).once('value',function(data){var v=data.val();v.$subkey=data.key;next(v);});},
 	/*SUBCOLLECTION end*//*COLLECTIONS start*/
 	getone:function(key,next){this.db(key.replace('-','/-')).once('value',function(d){var v=d.val();if(v){v.$key=key.split('-')[0]+d.key;next(v);}});},
-	add:function(otype,doc){if(f$.inoe(doc.title)){doc.title='new '+otype;}var x=this.db(otype).push(doc).key;this._add(f$.oxyprefix+'log',otype+x,{text:"Object Created"});
-	var nkey=otype+x;this._windex(doc,nkey,"title");return nkey;},
+	add:function(otype,doc){if(f$.inoe(doc[this.docnamefield])){doc[this.docnamefield]='new '+otype;}var x=this.db(otype).push(doc).key;this._add(f$.oxyprefix+'log',otype+x,{text:"Object Created"});
+	var nkey=otype+x;this._doindex(doc,nkey,this.docnamefield);return nkey;},
 	del:function(key){var _this=this;var doend=function(){_this.db('/'+f$.oxyprefix+'log_'+key.replace('-','/-')).remove();_this.db('/'+f$.oxyprefix+'ver_'+key.replace('-','/-')).remove();_this.db('/'+key.replace('-','/-')).remove();};
 		this.getone(key,function(d){for(var k in d.rels){_this.db(k.replace('-','/-')+'/rels/'+key).remove();
-			_this._add(f$.oxyprefix+'log',k,{text:'Unlinked from '+d.title+'['+key+'] because it\'s getting deleted.'});
+			_this._add(f$.oxyprefix+'log',k,{text:'Unlinked from '+d[_this.docnamefield]+'['+key+'] because it\'s getting deleted.'});
 		}doend();/*TODO CLEAN WINDEX*/});},
-	set:function(doc,log){var k=doc.$key;this._windex(doc,k,"title");var _this=this;delete doc.$key;if(!log){log='Object Modified'}
+	set:function(doc,log){var k=doc.$key;this._doindex(doc,k,this.docnamefield);var _this=this;delete doc.$key;if(!log){log='Object Modified'}
 			this.getone(k,function(d){delete d.$key;var verk=_this._add(f$.oxyprefix+"ver",k,d);
 			_this.db(k.replace('-','/-')).set(doc);_this._add(f$.oxyprefix+'log',k,{text:log});});},
 	/*COLLECTIONS end*//*RELATIONS start*/
 	link:function(k1,k2,json){if(!json){json={role:'default'}}if(f$.inoe(k1)||(f$.inoe(k2))){console.log('only valid keys')}else{
 		var _this=this;this.getone(k1,function(d){_this.getone(k2,function(dd){
-			_this.db(k1.replace('-','/-')+'/rels/'+k2).set(json);_this._add(f$.oxyprefix+'log',k1,{text:'Linked with '+dd.title+'['+k2+']'});
-			_this.db(k2.replace('-','/-')+'/rels/'+k1).set(json);_this._add(f$.oxyprefix+'log',k2,{text:'Linked with '+d.title+'['+k1+']'});
+			_this.db(k1.replace('-','/-')+'/rels/'+k2).set(json);_this._add(f$.oxyprefix+'log',k1,{text:'Linked with '+dd[_this.docnamefield]+'['+k2+']'});
+			_this.db(k2.replace('-','/-')+'/rels/'+k1).set(json);_this._add(f$.oxyprefix+'log',k2,{text:'Linked with '+d[_this.docnamefield]+'['+k1+']'});
 	});});}},
 	unlink:function(k1,k2){if(f$.inoe(k1)||(f$.inoe(k2))){console.log('only valid keys')}else{
 var _this=this;this.getone(k1,function(d){_this.getone(k2,function(dd){
-			_this.db(k1.replace('-','/-')+'/rels/'+k2).remove();_this._add(f$.oxyprefix+'log',k1,{text:'Unlinked from '+dd.title+'['+k2+']'});
-			_this.db(k2.replace('-','/-')+'/rels/'+k1).remove();_this._add(f$.oxyprefix+'log',k2,{text:'Unlinked from '+d.title+'['+k1+']'});
+			_this.db(k1.replace('-','/-')+'/rels/'+k2).remove();_this._add(f$.oxyprefix+'log',k1,{text:'Unlinked from '+dd[_this.docnamefield]+'['+k2+']'});
+			_this.db(k2.replace('-','/-')+'/rels/'+k1).remove();_this._add(f$.oxyprefix+'log',k2,{text:'Unlinked from '+d[_this.docnamefield]+'['+k1+']'});
 	});});}},	
 	linkmany:function(k1,kk,json){var _this=this;this.getone(k1,function(dd){for(var k=0;k<kk.length;k++){_this.getone(kk[k],function(ddd){
-			_this.db(k1.replace('-','/-')+'/rels/'+kk[k]).set(json);_this._add(f$.oxyprefix+'log',k1,{text:'Linked with '+ddd.title+'['+kk[k]+']'});
-			_this.db(kk[k].replace('-','/-')+'/rels/'+k1).set(json);_this._add(f$.oxyprefix+'log',kk[k],{text:'Linked with '+dd.title+'['+k1+']'});
+			_this.db(k1.replace('-','/-')+'/rels/'+kk[k]).set(json);_this._add(f$.oxyprefix+'log',k1,{text:'Linked with '+ddd[_this.docnamefield]+'['+kk[k]+']'});
+			_this.db(kk[k].replace('-','/-')+'/rels/'+k1).set(json);_this._add(f$.oxyprefix+'log',kk[k],{text:'Linked with '+dd[_this.docnamefield]+'['+k1+']'});
 	});}});},
 	unlinkmany:function(k1,kk){var _this=this;this.getone(k1,function(dd){for(var k=0;k<kk.length;k++){this.getone(kk[k],function(ddd){
-			_this.db(k1.replace('-','/-')+'/rels/'+kk[k]).remove();_this._add(f$.oxyprefix+'log',k1,{text:'Unlinked with '+ddd.title+'['+kk[k]+']'});
-			_this.db(kk[k].replace('-','/-')+'/rels/'+k1).remove();_this._add(f$.oxyprefix+'log',kk[k],{text:'Unlinked with '+dd.title+'['+k1+']'});
+			_this.db(k1.replace('-','/-')+'/rels/'+kk[k]).remove();_this._add(f$.oxyprefix+'log',k1,{text:'Unlinked with '+ddd[_this.docnamefield]+'['+kk[k]+']'});
+			_this.db(kk[k].replace('-','/-')+'/rels/'+k1).remove();_this._add(f$.oxyprefix+'log',kk[k],{text:'Unlinked with '+dd[_this.docnamefield]+'['+k1+']'});
 	});}});},/*RELATIONS end*//*WORD INDEX START*/
 	find:function(s,next,_collection){var _this=this;var popped=[];
-	 s=s.replace(/ |{|}|\||<|>|\\|!|"|ㄍ$|%|&|\/|\(|\)|=|\?|'|"|^|\*|\+|\[|\]|坑#|院@|\.|,|;|:/g,' ');
-    s=s.replace(/  /g,' ');s=s.replace(/   /g,' ');s=s.replace(/  /g,' ');
+	 s=s.replace(/ |{|}|\||<|>|\\|!|"|ㄍ$|%|&|\/|\(|\)|=|\?|'|"|^|\*|\+|\[|\]|坑院#|@|\.|,|;|:/g,' ');
+  s=s.replace(/# /g,' ');s=s.replace(/ */g,' ');/*s=s.replace(/   /g,' ');s=s.replace(/  /g,' ');*/
 		var step;var uninext=function(d){if(!popped[d.$key]){popped[d.$key]=true;next(d);}};
 		if(_collection){step=function(d){if(d.key.indexOf(_collection)==0){_this.getone(d.key,uninext);}}}
 		else{step=function(d){_this.getone(d.key,uninext);}}
 	 var xx=s.split(' ');var xlen=xx.length;for(var x=0;x<xlen;x++){
-		this.db('/'+f$.oxyprefix+'ndex/'+xx[x]+'/keys').on('child_added',step);}},
-	_windex:function(o,k,f){var xx=this.relevantText(o);var _this=this;
-		this.db('/'+f$.oxyprefix+'ndex_inv/'+k).once('value',function(data){var yy=data.val();if(yy){var zz=[];
+		this.db('/'+f$.oxyprefix+'Wndex/'+xx[x]+'/keys').on('child_added',step);}},
+	
+	_doindex:function(o,k,f){var RT=this._relevantText(o);var _this=this;var j;
+	 var IDX=this._indexAllandHashedWords(RT);
+		/* TODO URGENT : REMOVE OLD INDEX INFO ON REINDEX
+		this.db('/'+f$.oxyprefix+'Wndex_inv/'+k).once('value',function(data){var yy=data.val();if(yy){var zz=[];
 		//TODO:PERFORM Z=Y-X
-		var zlen=zz.length;for(var z=0;z<zlen;z++){_this.db('/'+f$.oxyprefix+'ndex/'+xx[x].v+'/keys/'+zz[z]).remove();}
-		}_this.db('/'+f$.oxyprefix+'ndex_inv/'+k).set(xx);});
-		for(var j in xx){if(xx[j].v!=''){this.db('/'+f$.oxyprefix+'ndex/'+xx[j].v+'/keys/'+k).set({ct:xx[j].c});}} 
- },
-	/*TODO:Add index only for hashed words */
-	/*TODO:Add index only for title words - use for autocomplete*/
-	relevantText:function(o){var s=JSON.stringify(o);var out={};var c=0;var cw='';var ct=1;
+		var zlen=zz.length;for(var z=0;z<zlen;z++){_this.db('/'+f$.oxyprefix+'Wndex/'+IDX.all[x].v+'/keys/'+zz[z]).remove();}
+		}_this.db('/'+f$.oxyprefix+'Wndex_inv/'+k).set(IDX.all);});
+		
+		this.db('/'+f$.oxyprefix+'Hndex_inv/'+k).once('value',function(data){var yy=data.val();if(yy){var zz=[];
+		//TODO:PERFORM Z=Y-X
+		var zlen=zz.length;for(var z=0;z<zlen;z++){_this.db('/'+f$.oxyprefix+'Wndex/'+IDX.hash[x].v+'/keys/'+zz[z]).remove();}
+		}_this.db('/'+f$.oxyprefix+'Hndex_inv/'+k).set(IDX.hash);});
+		*/
+		for(j in IDX.all){if(IDX.all[j].v!=''){this.db('/'+f$.oxyprefix+'Wndex/'+IDX.all[j].v+'/keys/'+k).set({ct:IDX.all[j].c});}}
+		for(j in IDX.hash){if(IDX.hash[j].v!=''){this.db('/'+f$.oxyprefix+'Hndex/'+IDX.hash[j].v+'/keys/'+k).set({ct:IDX.hash[j].c});}}
+		if(o[f]){this.db('/'+f$.oxyprefix+'Nndex/'+k).set({n:o[f]});}},
+	_relevantText:function(o){var s=JSON.stringify(o);
 		s=s.replace(/,"([^"]*)":/g,'');s=s.replace(/{"([^"]*)":/g,'');
 		s=s.replace(/","/g,' ');s=s.replace(/""/g,' ');s=s.replace(/"/g,' ');
-		//TODO URGENT:test for xml/html content - if detected remove entities  
-		s=s.replace(/ |{|}|\||<|>|\\|!|"|ㄍ$|%|&|\/|\(|\)|=|\?|'|"|^|\*|\+|\[|\]|坑#|院@|\.|,|;|:/g,' ');
+		s=s.replace(/<.*\/>/g,' ');
+		s=s.replace(/<!--.*-->/g,' ');
+		s=s.replace(/<.*>(.+)<\/.*>/g,' $1 ');
+		s=s.replace(/<link .*>/g,' ');
+		s=s.replace(/ |{|}|\||<|>|\\|!|"|ㄍ$|%|&|\/|\(|\)|=|\?|'|"|^|\*|\+|\[|\]|坑院@|\.|,|;|:/g,' ');
 		s=s.replace(/  /g,' ');s=s.replace(/   /g,' ');s=s.replace(/  /g,' ');
+		return s},	
+	_indexAllandHashedWords:function(s){var out={all:{},hash:{}};var c=0;var cw='';var ct=1;var gh=''
 		var ss=s.split(' ').sort();var len=ss.length;
 		while((c<len)&&(ss[c].trim()=='')){c++}
-		while(c<len){if(cw!=ss[c]){if(cw!=''){out[cw]={v:cw,c:ct};}cw=ss[c];ct=1;}else{ct++}c++}
-	out[cw]={v:cw,c:ct};return out;},/*WORD INDEX END*/	
-	}	
+		while(c<len){gh=ss[c].trim();if(cw!=gh){if(cw!=''){out.all[cw.replace('#','')]={v:cw.replace('#',''),c:ct};if(cw[0]=='#'){out.hash[cw.replace('#','')]={v:cw.replace('#',''),c:ct};}}cw=gh;ct=1;}else{ct++}c++}
+	 out.all[cw]={v:cw,c:ct};return out;
+	}
+		/*------------------------------------------------------------------------------------------------WORD INDEX END*/	
+	}
   };
