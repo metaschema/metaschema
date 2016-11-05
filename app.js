9a681da3b7a4f28b58284af9de53e7d400f97c98
Tritone <gioele@officinetesla.it>
2016-11-05 22:21:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -321,3 +321,19 @@ SETUP STEPS
 parent:-KKpBIMNFcVbEW0t2cqT
 
 */
+//TEMPORARY SCRIPT TO RESIZE TREE
+function addListeners(){
+	window.resizeBar=document.getElementById('resizebar');resizeBar.addEventListener('mousedown', mouseDown, false);window.addEventListener('mouseup', mouseUp, false);
+}
+function mouseUp()
+{
+    window.removeEventListener('mousemove', divMove, true);
+}
+function mouseDown(e){
+	var button=document.getElementsByClassName('fa-indent')[0] || document.getElementsByClassName('fa-dedent')[0]
+	button.classList.add('fa-dedent');button.classList.remove('fa-indent');document.body.classList.remove('noleft');window.addEventListener('mousemove', divMove, true);
+}
+function divMove(e){
+    var div = document.getElementById('resizebar');var left = document.getElementById('leftwrap');var wrap = document.getElementById('mainwrap');
+  div.style.left = e.clientX + 'px';wrap.style.left = e.clientX + 'px';left.style.width = e.clientX + 'px';
+}
